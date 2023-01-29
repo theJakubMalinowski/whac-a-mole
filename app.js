3ddc292e8639e8a540754855bae37ff199e909df
Jakub <malinowski.jakub@yahoo.com>
2023-01-29 16:00:13 -0600

    Moving square added to game board

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole ')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0 
+
+function randomSquare (){
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
+  let randomPosition = squares[Math.floor(Math.random()*9)]
+  console.log(randomPosition)
+}
+randomSquare()
+
